26016731ca88a2c1aed205cdc4aef3ff5af5390e
sourcegilmar <gilmardw@gmail.com>
2020-06-29 15:32:23 -0300

    Eliminação de espaçoes entre conceitos (ifs, métodos, blocos)

--- src/aplicacao/Atendimento.java
+++ src/aplicacao/Atendimento.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 public class Atendimento {
-	
 	private static class Atende {
 		public int cartao;
 		public String nome;
@@ -16,14 +15,12 @@ public class Atendimento {
 		double bonus;
 		public Atende prox;
 	}
-	
 	public static void main(String[] args) {
 
 		Atende inicio = null;
 		Atende fim = null;
 		Atende aux;
 		int op = 0;
-		
 		do {
 			try {
 				op = mostrarMenu();
@@ -36,68 +33,51 @@ public class Atendimento {
 			if (op < 1 || op > 13) {
 				System.out.println("Opção Inválida!!");
 			}
-
 			if (op == 1) {
 
 			}
-
 			if (op == 2) {
 
 			}
-
 			if (op == 3) {
 
 			}
-
 			if (op == 4) {
 
 			}
-
 			if (op == 5) {
 
 			}
-
 			if (op == 6) {
 
 			}
-
 			if (op == 7) {
 
 			}
-
 			if (op == 8) {
 
 			}
-
 			if (op == 9) {
 
 			}
-
 			if (op == 10) {
 
 			}
-
 			if (op == 11) {
 
 			}
-
 			if (op == 12) {
 
 			}
-			
 		} while (op !=13);
 		JOptionPane.showMessageDialog(null, "PROGRAMA FINALIZADO!");
-		
 	}
-	
     private static void agenteDeErro(NumberFormatException e) {
         JOptionPane.showMessageDialog(null, "ERRO AO TENTAR CONVERTER UM VALOR\nFAVOR VERIRIFICAR\nMENSAGEM ORIGINAL: " + e.getMessage(), "AGENTE DE ERRO", JOptionPane.CLOSED_OPTION);
     }
-
     private static void atendimentoVazio() {
         JOptionPane.showMessageDialog(null, "NÃO HÁ ATENDIMENTOS", "MENSAGEM DO PROGRAMA", JOptionPane.CLOSED_OPTION);
     }
-
     private static int mostrarMenu() {
         String texto = "\nMENU DE OPÇÕES\n"
                 + "\n1  - Recepcionar cliente"
@@ -113,11 +93,7 @@ public class Atendimento {
                 + "\n11 - Ver endereços hash"
                 + "\n12 - Sobre"
                 + "\n13 - Sair\n";
-
         int opcao = Integer.parseInt(JOptionPane.showInputDialog(texto, "1"));
         return opcao;
-
     }
-
-	
 }
